2c65e954cc9a7703c060b326b466ac6aa285621f
kevin <kevinothira@gmail.com>
2020-02-23 19:49:07 +0300

    added social media images

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+  $(".btn_1").click(function(){ 
+      $("p.par_1").show();
+      $(".btn_1").hide(); 
+  });
+  $("p.par_1").click(function(){
+      $("p.par_1").hide();
+      $(".btn_1").show(); 
+  });
+   $(".btn_2").click(function(){
+       $("p.par_2").show();   
+       $(".btn_2").hide();
+   });
+   $("p.par_2").click(function(){
+       $("p.par_2").hide();
+       $(".btn_2").show();
+   });
+   $(".btn_3").click(function(){
+       $("p.par_3").show();   
+       $(".btn_3").hide();
+   });
+   $("p.par_3").click(function(){
+       $(".btn_3").show();
+       $("p.par_3").hide();
+   }); 
+});
