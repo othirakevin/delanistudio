7a7384ba6f533f478e0228d7554cb2b99397d530
kevin <kevinothira@gmail.com>
2020-02-24 09:22:28 +0300

    aligned portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -27,28 +27,28 @@
 //function to toggle between the icons and the text
 $(document).ready(function(){
     
-    $(".btn_1").click(function(){ 
+    $(".button1").click(function(){ 
         $("p.par_1").show();
-        $(".btn_1").hide(); 
+        $(".button1").hide(); 
     });
     $("p.par_1").click(function(){
         $("p.par_1").hide();
-        $(".btn_1").show(); 
+        $(".button1").show(); 
     });
-     $(".btn_2").click(function(){
+     $(".button2").click(function(){
          $("p.par_2").show();   
-         $(".btn_2").hide();
+         $(".button2").hide();
      });
      $("p.par_2").click(function(){
          $("p.par_2").hide();
-         $(".btn_2").show();
+         $(".button2").show();
      });
-     $(".btn_3").click(function(){
+     $(".button3").click(function(){
          $("p.par_3").show();   
-         $(".btn_3").hide();
+         $(".button3").hide();
      });
      $("p.par_3").click(function(){
-         $(".btn_3").show();
+         $(".button3").show();
          $("p.par_3").hide();
      }); 
   });
