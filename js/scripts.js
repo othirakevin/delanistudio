1c5635d56877098f9790ef909fce855680d8fc8e
kevin <kevinothira@gmail.com>
2020-02-24 09:55:26 +0300

    centered background images

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,4 @@
-//  $(document).ready(function(){
-//          $(".submit").click(fuction(){
-//                    event.preventDefault();
-//                    console.log("Clicked button");
-        
-//                    var email = $(".email").val();
-//                    var name = $(".name").val();
-//                    var message = $(".message").val();
-//                    Var statusElm = $(".status");
-        
-//                    if(email.length > 5 && email.include("@") && email.include("."){
-//                        console.log("Email is valid");
-//                    });
-//                     else {
-//                       console.log("Email is not valid");
-//                   }
-//                });
-//        })
-    
-        
 
-
-
-
-
-
-//function to toggle between the icons and the text
 $(document).ready(function(){
     
     $(".button1").click(function(){ 
