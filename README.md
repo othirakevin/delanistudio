75702e6c5dda0ee4506b68716163f3fcc5ab87c1
kevin <kevinothira@gmail.com>
2020-02-24 07:10:01 +0300

    added a read me

---
+++ README.md
@@ -0,0 +1,33 @@
+# DELANI STUDIO PORTFOLIO.
+This is a portfolio project developed to show what services Delani studio offer 
+and collect feedback through email.
+
+
+## SetUp Instructions.
+The project need a web browser to display and offer interaction with the user
+and internet connection for a live feed and file submission.
+
+
+
+## Technology Used
+ > HTML
+ > JavaScript
+ > CSS
+ > JQuerry
+ > Bootstrap
+
+## Live link to the project.
+
+
+
+## Contact me:
+   Email: othirakevin@gmail.com
+   Twitter: @voke_kev
+   Facebook: kev voke othira
+
+
+
+## Licence
+The MIT licence
+Copyright (c)2019
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,56 @@
+//  $(document).ready(function(){
+//          $(".submit").click(fuction(){
+//                    event.preventDefault();
+//                    console.log("Clicked button");
+        
+//                    var email = $(".email").val();
+//                    var name = $(".name").val();
+//                    var message = $(".message").val();
+//                    Var statusElm = $(".status");
+        
+//                    if(email.length > 5 && email.include("@") && email.include("."){
+//                        console.log("Email is valid");
+//                    });
+//                     else {
+//                       console.log("Email is not valid");
+//                   }
+//                });
+//        })
+    
+        
+
+
+
+
+
+
+//function to toggle between the icons and the text
 $(document).ready(function(){
     
-  $(".btn_1").click(function(){ 
-      $("p.par_1").show();
-      $(".btn_1").hide(); 
+    $(".btn_1").click(function(){ 
+        $("p.par_1").show();
+        $(".btn_1").hide(); 
+    });
+    $("p.par_1").click(function(){
+        $("p.par_1").hide();
+        $(".btn_1").show(); 
+    });
+     $(".btn_2").click(function(){
+         $("p.par_2").show();   
+         $(".btn_2").hide();
+     });
+     $("p.par_2").click(function(){
+         $("p.par_2").hide();
+         $(".btn_2").show();
+     });
+     $(".btn_3").click(function(){
+         $("p.par_3").show();   
+         $(".btn_3").hide();
+     });
+     $("p.par_3").click(function(){
+         $(".btn_3").show();
+         $("p.par_3").hide();
+     }); 
   });
-  $("p.par_1").click(function(){
-      $("p.par_1").hide();
-      $(".btn_1").show(); 
-  });
-   $(".btn_2").click(function(){
-       $("p.par_2").show();   
-       $(".btn_2").hide();
-   });
-   $("p.par_2").click(function(){
-       $("p.par_2").hide();
-       $(".btn_2").show();
-   });
-   $(".btn_3").click(function(){
-       $("p.par_3").show();   
-       $(".btn_3").hide();
-   });
-   $("p.par_3").click(function(){
-       $(".btn_3").show();
-       $("p.par_3").hide();
-   }); 
-});
+
+  
