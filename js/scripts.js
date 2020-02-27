c3694f2db994afa4e11a555b5a17f08fb0d658b8
kevin <kevinothira@gmail.com>
2020-02-27 21:08:57 +0300

    added container to the whole html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,67 @@
 
-$(document).ready(function(){
-    
-    $(".button1").click(function(){ 
-        $("p.par_1").show();
-        $(".button1").hide(); 
-    });
-    $("p.par_1").click(function(){
-        $("p.par_1").hide();
-        $(".button1").show(); 
-    });
-     $(".button2").click(function(){
-         $("p.par_2").show();   
-         $(".button2").hide();
-     });
-     $("p.par_2").click(function(){
-         $("p.par_2").hide();
-         $(".button2").show();
-     });
-     $(".button3").click(function(){
-         $("p.par_3").show();   
-         $(".button3").hide();
-     });
-     $("p.par_3").click(function(){
-         $(".button3").show();
-         $("p.par_3").hide();
-     }); 
-  });
 
+// $(document).ready(function () {
+//     $('#toggle').click(function () {
+//         $('#p1').show();
+//     });
+//     $('#toggle').click(function () {
+//         $('#toggle').hide();
+//     });
+//     $('#p1').click(function () {
+//         $('#toggle').show();
+//     });
+// $('#p1').click(function () {
+//         $('#p1').hide();
+//     });
+//     $('#toggle2').click(function () {
+//         $('#p2').show();
+//     });
+//     $('#toggle2').click(function () {
+//         $('#toggle2').hide();
+//     });
+//     $('#p2').click(function () {
+//         $('#toggle2').show();
+//     });
+//     $('#p2').click(function () {
+//         $('#p2').hide();
+//     });
+//     $('#toggle3').click(function () {
+//         $('#p3').show();
+//     });
+//     $('#toggle3').click(function () {
+//         $('#toggle3').hide();
+//     });
+//     $('#p3').click(function () {
+//         $('#toggle3').show();
+//     });
+//     $('#p3').click(function () {
+//         $('#p3').hide();
+//     });
 
+//   $(document).ready(function () {
+//     $("form").submit(function (event) {
+//         var name = document.getElementById("name").value;
+//         var email = document.getElementById("email").value;
+//         if (name == "" || email == "") {
+//             alert("Cannot submit without name and email");
+//         } else {
+//             alert("Thank you " + name + " \n Message submitted successfully");
+//         }
+//         event.preventDefault();
+//     });
+// });
 
-  $(document).ready(function () {
-    $("form").submit(function (event) {
-        var name = document.getElementById("name").value;
-        var email = document.getElementById("email").value;
-        if (name == "" || email == "") {
-            alert("Cannot submit without name and email");
-        } else {
-            alert("Thank you " + name + " \n Message submitted successfully");
-        }
-        event.preventDefault();
-    });
-});
-
-  
+$(document).ready(function() {
+    $(".design").click(function() {
+        $("#design-show").toggle()
+        $("#design-hide").toggle()
+    })
+    $(".dev").click(function() {
+        $("#dev-show").toggle()
+        $("#dev-hide").toggle()
+    })
+    $(".product").click(function() {
+        $("#product-show").toggle()
+        $("#product-hide").toggle()
+    })
+})
